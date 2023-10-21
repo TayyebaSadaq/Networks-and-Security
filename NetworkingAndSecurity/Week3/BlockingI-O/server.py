dcac9682257e9a51d9c881e4711e4ce7f14d68ff
Tayyeba <official.tayyeba@gmail.com>
2023-10-21 16:41:39 +0100

    update server

--- NetworkingAndSecurity/Week3/BlockingI-O/server.py
+++ NetworkingAndSecurity/Week3/BlockingI-O/server.py
@@ -2,3 +2,25 @@ import logging
 import socket
 import select
 
+### SET UP LISTENING SOCKET ###
+server_hostname = socket.gethostname() # change this to the server's hostname/IP address
+server_port = 6060 # change this to the server's port number
+
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+logging.info("Blocking - creating socket")
+
+logging.info("Blocking - connecting")
+
+s.connect((server_hostname, server_port))
+logging.info("Blocking - connected")
+
+logging.info("Blocking - sending...")
+s.send(b"Hello, world!")
+
+logging.info("Blocking - waiting...")
+data = s.recv(1024)
+logging.info(f"Blocking = data = {len(data)}")
+
+logging.info("Blocking - closing socket")
+s.close()
