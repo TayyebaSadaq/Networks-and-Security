8643cc93bdc7e7ccedef2c6cacd63c4e93f3678b
Tayyeba <official.tayyeba@gmail.com>
2023-10-22 21:02:15 +0100

    update file

--- NetworkingAndSecurity/Week3/Subprocess/subprocess1.py
+++ NetworkingAndSecurity/Week3/Subprocess/subprocess1.py
@@ -1,4 +1,10 @@
 import subprocess
 
-subprocess.run("echo hello", shell=True)
+res = subprocess.run("echo Hello", shell=True)
+
+print("------------------")
+
+# print(dir(res)) # show all the attributes and methods of the object
+
+print(*dir(res), sep = "\n") # show all the attributes and methods of the object in a list format (one per line)
 
