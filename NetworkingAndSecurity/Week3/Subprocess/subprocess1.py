c705ce63aecd291b585a3819cb25e036d6179460
Tayyeba <official.tayyeba@gmail.com>
2023-10-22 21:10:58 +0100

    save and exit

--- NetworkingAndSecurity/Week3/Subprocess/subprocess1.py
+++ NetworkingAndSecurity/Week3/Subprocess/subprocess1.py
@@ -1,10 +1,19 @@
 import subprocess
 
-res = subprocess.run("echo Hello", shell=True)
+res = subprocess.run(
+    "echo Hello", 
+    shell=True,
+    capture_output = True,
+    )
 
 print("------------------")
 
 # print(dir(res)) # show all the attributes and methods of the object
 
 print(*dir(res), sep = "\n") # show all the attributes and methods of the object in a list format (one per line)
-
+print("------------------")
+print(res.stdout) # None means no output
+print("------------------")
+print(res.stderr) # None means no error
+print("------------------") 
+print(res.returncode) # 0 means success, 1 means failure
