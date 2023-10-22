b4512028d8ec4ed765c180cf31877aa21cfb2ffc
Tayyeba <official.tayyeba@gmail.com>
2023-10-22 19:17:20 +0100

    added all code snippits

---
+++ NetworkingAndSecurity/Week3/MessageEntryPoint/app-server.py
@@ -0,0 +1,17 @@
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    message = libserver.Message(sel, conn, addr)
+    sel.register(conn, selectors.EVENT_READ, data=message)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    message.process_events(mask)
---
+++ NetworkingAndSecurity/Week3/MessageEntryPoint/libclient.py
@@ -0,0 +1,14 @@
+class Message:
+    def __init__(self, selector, sock, addr, request):
+        # ...
+
+    def write(self):
+        if not self._request_queued:
+            self.queue_request()
+
+        self._write()
+
+        if self._request_queued:
+            if not self._send_buffer:
+                # Set selector to listen for read events, we're done writing.
+                self._set_selector_events_mask("r")
