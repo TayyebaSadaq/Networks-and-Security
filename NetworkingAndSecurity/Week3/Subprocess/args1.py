acfa87661d72759f68c113371ef656743c1dc520
Tayyeba <official.tayyeba@gmail.com>
2023-10-23 16:52:53 +0100

    update

--- NetworkingAndSecurity/Week3/Subprocess/args1.py
+++ NetworkingAndSecurity/Week3/Subprocess/args1.py
@@ -1,3 +1,12 @@
 import sys
 
-print(sys.argv) # print the command line arguments
+args = sys.argv[1:]
+
+if "-h" in args or "--help" in args: # if the user wants help
+    print("Progress to print args.") # print the help text
+    print("Options")
+    print("-h or --help: print this help text")
+    exit(0)
+
+for arg in args:
+    print(arg)
