c28f557cc6480a007e1a0eb9eaf9ffc98ecb75bc
Tayyeba <official.tayyeba@gmail.com>
2023-10-24 13:35:27 +0100

    update

---
+++ NetworkingAndSecurity/Week4/daemon_thread.py
@@ -0,0 +1,30 @@
+import logging #for logging messages
+import threading #for logging messages
+import time #for sleep
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name) #logs a message when the thread starts 
+    time.sleep(2) #stimulate some work by sleeping for 2 seconds
+    logging.info("Thread %s: finishing", name) #logs a message when the thread finishes 
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s" #sets the log messafge format adn logging level 
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    #log a message before creating the thread
+    logging.info("Main    : before creating thread")
+    #create a nre thread with the thread_function and arguements
+    x = threading.Thread(target=thread_function, args=(1,), daemon=True) 
+    # The 'daemon' flag is set to True, so this thread is a daemon thread.
+    # It won't prevent the program from exiting if the main program is done.
+    # If the main program exits before this thread completes, the thread will be abruptly terminated.
+
+    #log a message before starting the thread
+    logging.info("Main    : before running thread")
+    x.start() #start the thread
+    logging.info("Main    : wait for the thread to finish")
+    #log a message indicating that the main program is waiting to finish 
+    # x.join() #uncomment if you want the main program to complete and then join 
+    logging.info("Main    : all done")
+    #log a message to indicate that all actions are done and it may still be running as x.join is still commented out 
+
