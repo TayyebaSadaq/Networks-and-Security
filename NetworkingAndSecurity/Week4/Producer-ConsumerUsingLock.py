1e2c96e40250a98c866dc32c969bb2a9aa22d1aa
Tayyeba <135642166+TayyebaSadaq@users.noreply.github.com>
2023-10-25 20:25:26 +0000

    update file and created file

--- NetworkingAndSecurity/Week4/Producer-ConsumerUsingLock.py
+++ NetworkingAndSecurity/Week4/Producer-ConsumerUsingLock.py
@@ -14,28 +14,29 @@ class Pipeline:
     """
 
     def __init__(self):
-        self.message = 0
-        self.producer_lock = threading.Lock()
-        self.consumer_lock = threading.Lock()
+        self.message = 0 # stores message to pass
+        # these are both threading.Lock objects
+        self.producer_lock = threading.Lock() # restricts access to the message by the producer thread
+        self.consumer_lock = threading.Lock() # restricts access to the message by the consumer thread
         self.consumer_lock.acquire()
 
     def get_message(self, name):
         logging.debug("%s:about to acquire getlock", name)
-        self.consumer_lock.acquire()
+        self.consumer_lock.acquire() # make consumer wait until a message is ready
         logging.debug("%s:have getlock", name)
-        message = self.message
+        message = self.message # get message
         logging.debug("%s:about to release setlock", name)
-        self.producer_lock.release()
+        self.producer_lock.release() # allows the producer to insert the next message into the pipeline
         logging.debug("%s:setlock released", name)
         return message
 
     def set_message(self, message, name):
         logging.debug("%s:about to acquire setlock", name)
-        self.producer_lock.acquire()
+        self.producer_lock.acquire() # make producer wait until consumer is done with previous message
         logging.debug("%s:have setlock", name)
-        self.message = message
+        self.message = message # set message
         logging.debug("%s:about to release getlock", name)
-        self.consumer_lock.release()
+        self.consumer_lock.release() # allows consumer to retrieve next message from pipeline
         logging.debug("%s:getlock released", name)
 
 
@@ -50,7 +51,7 @@ def producer(pipeline): # Producer thread reads from network and puts message in
     pipeline.set_message(SENTINEL, "Producer")
 
 
-def consumer(pipeline):
+def consumer(pipeline): # reads message from pipeline and writes it to a fake database, printing it out
     """Pretend we're saving a number in the database."""
     message = 0
     while message is not SENTINEL:
@@ -61,10 +62,10 @@ def consumer(pipeline):
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    # logging.getLogger().setLevel(logging.DEBUG)
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S") # configure logging
+    logging.getLogger().setLevel(logging.DEBUG) # see exactly where each thread acquires and releases the locks
 
-    pipeline = Pipeline()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    pipeline = Pipeline() # create pipeline object
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor: # create thread pool with 2 threads
         executor.submit(producer, pipeline)
         executor.submit(consumer, pipeline)
